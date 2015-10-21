c0b597e1274083bc26347c137a8a8d0492b060b2
Jordan-Queiroz <jsq@icomp.ufam.edu.br>
2015-10-21 19:49:18 -0400

    Sorting students by grade.

--- report/script.js
+++ report/script.js
@@ -1,10 +1,28 @@
 $.post('report.php', {}, function(data){
 
-	var students = []
+	var students = [];
 
 	var students_array = JSON.parse(data);
 	for(var i = 0; i < students_array.length; i++){
 		students.push(JSON.parse(students_array[i]));
+	}
+
+	students.sort(sortByProperty('grade'));
+	
+	console.log(students);
+
+	function sortByProperty(property) {
+    	'use strict';
+    	return function (a, b) {
+        	var sortStatus = 0;
+        	if (a[property] < b[property]) {
+            	sortStatus = 1;
+        	} else if (a[property] > b[property]) {
+            	sortStatus = -1;
+        	}
+ 
+        	return sortStatus;
+    	};
 	}
 	//console.log(students);
 });
